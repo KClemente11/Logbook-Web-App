640f539cd086e4e61693d60e1921fa7302bf96cd
Kyle Clemente <kclemente11@me.com>
2023-07-14 22:11:35 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Logbook_Web_App.Models;
+
+namespace Logbook_Web_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Logbook_Web_App.Models.Log> Log { get; set; } = default!;
+    }
+}
---
+++ Models/Log.cs
@@ -0,0 +1,13 @@
+﻿namespace Logbook_Web_App.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string LogTitle { get; set; }
+        public string LogDescription { get; set; }
+        public Log()
+        {
+            
+        }
+    }
+}
